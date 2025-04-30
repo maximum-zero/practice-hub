7c2e9e69cfcc9a861edecc8762dd10b44aed0bb3
maximum0 <maximum.zero95@gmail.com>
2025-05-01 00:47:15 +0900

    feat: initial commit

---
+++ password-generator/src/main/java/com/maximum0/passwordgenerator/PasswordGenerator.java
@@ -0,0 +1,8 @@
+package com.maximum0.passwordgenerator;
+
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ spring-starter/src/test/java/com/maximum0/starter/SpringStarterApplicationTest.java
@@ -0,0 +1,13 @@
+package com.maximum0.starter;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringStarterApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
