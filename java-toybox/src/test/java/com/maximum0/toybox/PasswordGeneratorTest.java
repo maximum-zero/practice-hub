019733ed2a9c763425030fce72b5eb2bac9cc5c6
maximum0 <maximum.zero95@gmail.com>
2025-05-01 12:26:40 +0900

    test(java-toybox/passwordgenerator): 비밀번호 길이 경계 테스트 추가

--- java-toybox/src/test/java/com/maximum0/toybox/PasswordGeneratorTest.java
+++ java-toybox/src/test/java/com/maximum0/toybox/PasswordGeneratorTest.java
@@ -2,22 +2,26 @@ package com.maximum0.toybox;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class PasswordGeneratorTest {
 
     @DisplayName("비밀번호가 최소 8자 이상 12자 이하면 예외가 발생하지 않습니다.")
-    @Test
-    void validatePasswordTest() {
-        assertThatCode(() -> PasswordGenerator.validate("일이삼사오육칠팔"))
+    @ParameterizedTest
+    @ValueSource(strings = {"일이삼사오육칠팔", "일이삼사오육일팔구십일이"})
+    void validatePasswordTest(String password) {
+        assertThatCode(() -> PasswordGenerator.validate(password))
             .doesNotThrowAnyException();
     }
 
     @DisplayName("비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킵니다.")
-    @Test
-    void validatePasswordTestByException() {
-        assertThatCode(() -> PasswordGenerator.validate("일이삼사오육칠"))
+    @ParameterizedTest
+    @ValueSource(strings = {"일이삼", "일이삼사오육", "일이삼사오육일팔구십일이삼", "일이삼사오육일팔구십일이삼사오육칠팔구십"})
+    void validatePasswordTestByException(String password) {
+        assertThatCode(() -> PasswordGenerator.validate(password))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("비밀번호가 최소 8자 이상 12자 이하여야 합니다.");
     }
