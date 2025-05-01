bd02028934ea655ddf9e3978dd498f74ef94c31e
maximum0 <maximum.zero95@gmail.com>
2025-05-01 18:18:17 +0900

    refactor(java-toybox/calculator): PositiveNumber 도입으로 Divide 연산의 유효성 검사 제거

--- java-toybox/src/main/java/com/maximum0/calculator/operator/DivisionArithmeticOperator.java
+++ java-toybox/src/main/java/com/maximum0/calculator/operator/DivisionArithmeticOperator.java
@@ -10,9 +10,6 @@ public class DivisionArithmeticOperator implements IArithmeticOperator {
 
     @Override
     public int calculate(PositiveNumber operand1, PositiveNumber operand2) {
-        if (operand2.toInt() < 1) {
-            throw new IllegalArgumentException("0 또는 음수를 전달할 수 없습니다.");
-        }
         return operand1.toInt() / operand2.toInt();
     }
 }
