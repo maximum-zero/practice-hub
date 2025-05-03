b1121d7772f4d69d6472eec3d39388e9193149bb
maximum0 <maximum.zero95@gmail.com>
2025-05-04 00:11:45 +0900

    feat(java-toybox/was-practice): 고정 ThreadPool 적용하여 요청 처리 성능 개선

--- was-practice/src/main/java/org/maximum0/WebApplicationServer.java
+++ was-practice/src/main/java/org/maximum0/WebApplicationServer.java
@@ -9,9 +9,12 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebApplicationServer {
     private final int port;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
 
@@ -29,29 +32,7 @@ public class WebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 log.info("[WebApplicationServer] client connected.");
 
-                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryString();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        log.debug("result = " + result);
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
