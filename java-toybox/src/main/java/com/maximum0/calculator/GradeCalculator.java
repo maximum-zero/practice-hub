ca31c8a7448a72bf443d9063af6502d114a2a86a
maximum0 <maximum.zero95@gmail.com>
2025-05-03 19:47:15 +0900

    refactor(java-toybox/calculator): 평균 학점 계산 일급 컬렉션을 이용해 코드 리펙토링

--- java-toybox/src/main/java/com/maximum0/calculator/GradeCalculator.java
+++ java-toybox/src/main/java/com/maximum0/calculator/GradeCalculator.java
@@ -1,23 +1,19 @@
 package com.maximum0.calculator;
 
 import com.maximum0.calculator.domain.Course;
+import com.maximum0.calculator.domain.Courses;
 
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     public double calculator() {
-        double total = 0.0;
-        int count = 0;
-        for (Course course : courses) {
-            total += course.multiplyCreditAndGrade();
-            count += course.getCredit();
-        }
-        return total / count;
+        // 평균 학점 계산 = (학점수 x 교과목 평점)의 합계 / 수강신청 총 학점 수
+        return courses.multiplyCreditAndCourseGrade() / courses.calculateTotalCompletedCredit();
     }
 }
--- java-toybox/src/test/java/com/maximum0/calculator/GradeCalculatorTest.java
+++ java-toybox/src/test/java/com/maximum0/calculator/GradeCalculatorTest.java
@@ -13,10 +13,8 @@ public class GradeCalculatorTest {
     @DisplayName("평균 학점을 계산합니다.")
     @Test
     void calculateTest() {
-        // "평균 학점 계산 = (학점수 x 교과목 평점)의 합계 / 수강신청 총 학점 수"
         GradeCalculator gradeCalculator = new GradeCalculator(List.of(new Course("OOP", 3, "A"), new Course("자료구조", 5, "A")));
         double result = gradeCalculator.calculator();
         assertThat(result).isEqualTo(4);
-
     }
 }
