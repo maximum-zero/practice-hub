242f3774df7018b6eb1a094858dddd6021ec73a9
maximum0 <maximum.zero95@gmail.com>
2025-05-04 15:25:20 +0900

    refactor(servlet-practice): Servlet → HttpServlet으로 리팩토링

--- servlet-practice/src/main/java/org/maximum0/CalculatorServlet.java
+++ servlet-practice/src/main/java/org/maximum0/CalculatorServlet.java
@@ -5,25 +5,19 @@ import org.maximum0.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet implements Servlet {
+public class CalculatorServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
-    private ServletConfig servletConfig;
 
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-        log.info("init");
-        this.servletConfig = servletConfig;
-    }
-
-    @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        log.info("service");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int operand1 = Integer.parseInt(request.getParameter("operand1"));
         String operator = request.getParameter("operator");
         int operand2 = Integer.parseInt(request.getParameter("operand2"));
@@ -33,18 +27,4 @@ public class CalculatorServlet implements Servlet {
         writer.println(result);
     }
 
-    @Override
-    public ServletConfig getServletConfig() {
-        return this.servletConfig;
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-        // Resource Release
-    }
 }
